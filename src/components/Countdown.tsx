381e996991d36433ff23a57ce72eeb9fe31367fd
ian <iancmilan@unochapeco.edu.br>
2021-02-26 10:59:56 -0300

    melhorando a usabilidade da aplicação ♿️

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -1,19 +1,18 @@
 import { useState, useEffect, useContext } from 'react';
 
 import { ChallengesContext } from '../contexts/ChallengesContext';
+import { CountdownContext } from '../contexts/CountdownContext';
 import styles from '../styles/components/Countdown.module.css';
 
-let countdownTimeout: NodeJS.Timeout;
-
 export function Countdown() {
-    const { startNewChallenge } = useContext(ChallengesContext);
-
-    const [ time, setTime ] = useState(0.05 * 60);
-    const [ isActive, setIsActive ] = useState(false);
-    const [ hasFinished, setHasFinished ] = useState(false);
-
-    const minutes = Math.floor(time / 60); // sempre vai arredondar os minutos para baixo, ex: se o resultado de time/60 for 24.5 a const minutes será 24
-    const seconds = time % 60; // resto da divisão
+    const { 
+        minutes, 
+        seconds, 
+        hasFinished, 
+        isActive, 
+        startCountdown, 
+        resetCountdown 
+    } = useContext(CountdownContext);
 
     //string(minutes) tranformará o numero em string. Ex: 25 = '25'
     // padStart vai verificar se nossa string tem 2 caracteres e caso não tenha preencherá a esquerda com 0. Ex: 5 minutos na função String(minutes) será '5', o padStart vai verificar se a lenght é menor 2, vai ser true, o resultado disso será '05', e no final o split fará isso ficar ['0', '5']
@@ -21,28 +20,6 @@ export function Countdown() {
 
     const [ minuteLeft, minuteRight ] = String(minutes).padStart(2, '0').split('');
     const [ secondLeft, secondRight ] = String(seconds).padStart(2, '0').split('');
-
-    function startCountdown() {
-        setIsActive(true);
-    }
-
-    function resetCountdown() {
-        clearTimeout(countdownTimeout);
-        setIsActive(false);
-        setTime(0.05 * 60);
-    }
-
-    useEffect(() => {
-        if (isActive && time > 0) {
-            countdownTimeout = setTimeout(() => {
-                setTime(time - 1);
-            }, 1000);
-        } else if (isActive && time == 0) {
-            setHasFinished(true);
-            setIsActive(false);
-            startNewChallenge();
-        }
-    }, [isActive, time]);
 
     return(
         <div>
